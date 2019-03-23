1c989d471a4594d651f72451c701617d2a0c083c
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-23 20:36:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 function produceDrivingRange(blockRange) {
     return function (x, y) {
-      if(Math.abs(x-y)>=blockRange) {
-        return `within range by ${Math.abs(x-y)}`;
-      } else {
-          return `${Math.abs(x-y)} out of range`;
-      }
+      return x-y;
     }
   }
