7af5283566702f18c8682fa3eea4a649a84ebccf
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-23 20:44:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ function produceDrivingRange(blockRange) {
       if(Math.abs(parseInt(x)-parseInt(y)) <= blockRange) {
         return `within range by ${blockRange - Math.abs(parseInt(x)-parseInt(y))}`;
       }
+      else {
+        return `${blockRange - Math.abs(parseInt(x)-parseInt(y))} out of range`;
+      }
     }
   }
